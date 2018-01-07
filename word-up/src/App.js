f53dd61433ea4fd7cd84672e85a59599ebd75e73
Adair Hawkins <adairhawkins@gmail.com>
2018-01-07 15:05:25 -0600

    reacstrap implemented with Wrapper
    Wrapper component within contrainer.js file is functioning (on my computer)

--- word-up/src/App.js
+++ word-up/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HangmanBox from "./Components/HangmanBox";
+import Wrapper from "./Components/container";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HangmanBox />
+        <Wrapper />
       </div>
     );
   }
