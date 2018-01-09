68a598eadb83e89ea92373713298ff7f10633195
Adair Hawkins <adairhawkins@gmail.com>
2018-01-09 11:41:50 -0600

    images
    I downloaded the images from wikapedia, and I got closish to the routing. I remembered to import them in, but something is still wrong.

--- word-up/src/Components/container.js
+++ word-up/src/Components/container.js
@@ -1,84 +1,86 @@
-import React, { Component } from 'react';
-import {Container, Row, Col, Card, CardImg, CardBody, CardTitle, CardText, CardSubtitle, Button} from 'reactstrap';
-
+import React, {Component} from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  Button
+} from 'reactstrap';
+import './images/Hangman_game.jpg';
+import './images/Hangman-0.png';
 
 class ContainerComponent extends Component {
   render() {
-    return (
-      <div>
-        <Container fluid = {true}>
-          <Row>
-            <Col>
-              <h1>Hangman Div</h1>
-              <p>
-                <Button
-                  tag="a"
-                  color="success"
-                  size="large"
-                  href="http://reactstrap.github.io"
-                  target="_blank"
-                >
-                  View Reactstrap Docs
-                </Button>
-              </p>
-            </Col>
-            <Col>
-              <Row>
-                <Col>
-                </Col>
-                <Col>
-                  <h1>Hints!</h1>
-                </Col>
-                <Col>
-                </Col>
-              <Row>
-                <Col>
-                  <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=300&h=275" alt="Card image cap" />
-                  </Card>
-                </Col>
-                <Col>
-                  <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=300&h=275" alt="Card image cap" />
-                  </Card>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=300&h=275" alt="Card image cap" />
-                  </Card>
-                </Col>
-                <Col>
-                  <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=300&h=275" alt="Card image cap" />
-                  </Card>
-                </Col>
-              </Row>
-              </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-            </Col>
-            <Col>
-              <Button
-                tag="a"
-                color="success"
-                size="large"
-                href="http://reactstrap.github.io"
-                target="_blank"
-              >
-                Play Again!
-              </Button>
-            </Col>
-            <Col>
-            </Col>
-          </Row>
+    return (<div>
+      <Container fluid={true}>
+        <Row>
+          <Col>
+            <h1>Hangman Div</h1>
+            <Row>
+              <Col>
+                <Card>
+                  <CardImg top="top" width="100%" src="./images/Hangman_game.jpg" alt="Card image cap"/>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <CardImg top="top" width="100%" src="./Hangman-0.png" alt="Card image cap"/>
+                </Card>
+              </Col>
+            </Row>
+          </Col>
+        <Col>
 
-        </Container>
-      </div>
-    )
+  {/* Giphy */}
+            <Row>
+              <Col></Col>
+              <Col>
+                <h1>Hints!</h1>
+              </Col>
+              <Col></Col>
+              <Row>
+                <Col>
+                  <Card>
+                    <CardImg top="top" width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=300&h=275" alt="Card image cap"/>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card>
+                    <CardImg top="top" width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=300&h=275" alt="Card image cap"/>
+                  </Card>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Card>
+                    <CardImg top="top" width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=300&h=275" alt="Card image cap"/>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card>
+                    <CardImg top="top" width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=300&h=275" alt="Card image cap"/>
+                  </Card>
+                </Col>
+              </Row>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col>
+            <Button tag="a" color="success" size="large" href="http://reactstrap.github.io" target="_blank">
+              Play Again!
+            </Button>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>)
   }
 }
 
